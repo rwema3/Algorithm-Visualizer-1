afa081ca2fde1634e031dd0b09348e0472518319
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-01-27 13:51:35 +0200

    Widget build(BuildContext context)

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,6 @@ import 'package:flutter/material.dart';
 
 
 
-  Widget build(BuildContext context) {
-    ListTile makeListTile(Lesson lesson) => ListTile(
-		contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-          leading: Container(
-            padding: EdgeInsets.only(right: 12.0),
-			  decoration: new BoxDecoration(border: new Border(right: new BorderSide(width: 1.0, color: Colors.white24))),
-            child: lesson.icon,
-          ),
-          title: Text(
-            lesson.title,
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
           // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
 
           subtitle: Row(
