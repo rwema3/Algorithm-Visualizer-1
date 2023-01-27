48135c72cebc893b091ba5a19d118264c83c1502
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-01-27 13:53:29 +0200

    void mains

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:AlgorithmVisualizer/model/lesson.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(new MyApp());
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  final Controllers _controllers = Controllers();
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
         primaryColor: Color.fromRGBO(58, 66, 86, 1.0),
