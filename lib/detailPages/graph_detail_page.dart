c0e7a49a59a40bf394a555f6e9bb768dc52c37c9
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-05 00:22:59 +0200

    EdgeInsets & symmetric

--- lib/detailPages/graph_detail_page.dart
+++ lib/detailPages/graph_detail_page.dart
@@ -33,3 +33,41 @@ class GraphHomePage extends HomePage {
   Container getNumberOfEdgesSlider(BuildContext context) {
     return askForInformation(lesson.simulationDetails, lesson.askForEdges)
         ? Container(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: Slider(
+                    activeColor: Colors.green,
+                    min: minEdges(),
+                    max: maxEdges(),
+                    onChanged: (value) {
+                      setState(() {
+                        return lesson.edges = value;
+                      });
+                    },
+                    value: minMaxEdges(),
+                  ),
+                ),
+                Container(
+                  width: 80.0,
+                  alignment: Alignment.center,
+                  child: Text('${lesson.edges.toInt()}', style: Theme.of(context).textTheme.display1),
+                ),
+              ],
+            ))
+        : Container();
+  }
+
+  Container getNumberOfEdgesText(BuildContext context) {
+    return askForInformation(lesson.simulationDetails, lesson.askForEdges)
+        ? Container(
+            padding: EdgeInsets.fromLTRB(10.0, 32.0, 0.0, 0.0),
+            width: MediaQuery.of(context).size.width,
+            child: Text(getEdgesMessage(), style: TextStyle(color: Colors.black)),
+          )
+        : Container();
+  }
